4b4f111cd4bde6ec4fe4ffce3f868994bfa03441
Siddharth Dubey <122088830+sidcr7-likeminds@users.noreply.github.com>
2023-12-21 12:38:21 +0530

    create view and style for text

--- likeminds-feed-android-ui/src/main/java/com/likeminds/feed/android/ui/base/styles/LMFeedTextStyle.kt
+++ likeminds-feed-android-ui/src/main/java/com/likeminds/feed/android/ui/base/styles/LMFeedTextStyle.kt
@@ -1,12 +1,83 @@
 package com.likeminds.feed.android.ui.base.styles
 
+import android.graphics.Typeface
+import android.text.TextUtils.TruncateAt
+import com.likeminds.feed.android.ui.R
 import com.likeminds.feed.android.ui.base.views.LMFeedTextView
 import com.likeminds.feed.android.ui.utils.ViewStyle
 
-class LMFeedTextStyle : ViewStyle {
+class LMFeedTextStyle private constructor(
+    val textColor: Int,
+    val textSize: Int,
+    val fontResource: Int?,
+    val fontAssetsPath: String?,
+    val typeface: Int,
+    val maxLines: Int?,
+    val ellipsize: TruncateAt?
+) : ViewStyle {
+
+    class Builder {
+        private var textColor: Int = R.color.colorPrimary
+        private var textSize: Int = R.dimen.lm_feed_ui_text_small
+        private var fontResource: Int? = null
+        private var fontAssetsPath: String? = null
+        private var typeface: Int = Typeface.NORMAL
+        private var maxLines: Int? = null
+        private var ellipsize: TruncateAt? = null
+
+        fun textColor(textColor: Int) = apply {
+            this.textColor = textColor
+        }
+
+        fun textSize(textSize: Int) = apply {
+            this.textSize = textSize
+        }
+
+        fun fontResource(fontResource: Int?) = apply {
+            this.fontResource = fontResource
+        }
+
+        fun fontAssetsPath(fontAssetsPath: String?) = apply {
+            this.fontAssetsPath = fontAssetsPath
+        }
+
+        fun typeface(typeface: Int) = apply {
+            this.typeface = typeface
+        }
+
+        fun maxLines(maxLines: Int?) = apply {
+            this.maxLines = maxLines
+        }
+
+        fun ellipsize(ellipsize: TruncateAt?) = apply {
+            this.ellipsize = ellipsize
+        }
+
+        fun build() = LMFeedTextStyle(
+            textColor,
+            textSize,
+            fontResource,
+            fontAssetsPath,
+            typeface,
+            maxLines,
+            ellipsize
+        )
+    }
+
+    fun toBuilder(): Builder {
+        return Builder().textColor(textColor)
+            .textSize(textSize)
+            .fontResource(fontResource)
+            .fontAssetsPath(fontAssetsPath)
+            .typeface(typeface)
+            .maxLines(maxLines)
+            .ellipsize(ellipsize)
+    }
 
     fun apply(textView: LMFeedTextView) {
-
+        textView.apply {
+            this.textSize = this@LMFeedTextStyle.textSize.toFloat()
+        }
     }
 }
 
--- likeminds-feed-android-ui/src/main/java/com/likeminds/feed/android/ui/base/views/LMFeedTextView.kt
+++ likeminds-feed-android-ui/src/main/java/com/likeminds/feed/android/ui/base/views/LMFeedTextView.kt
@@ -1,4 +1,15 @@
 package com.likeminds.feed.android.ui.base.views
 
-class LMFeedTextView {
-}
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatTextView
+
+/**
+ * Represents a basic text view
+ * To customize this view use [LMFeedTextStyle]
+ */
+class LMFeedTextView(
+    context: Context,
+    attrs: AttributeSet?,
+    defStyle: Int
+) : AppCompatTextView(context, attrs, defStyle)
