fc2ce63a5b7409683c07bf9b03bd008eb74cdba9
Ishaan Jain <ishaan.jain@likeminds.community>
2024-03-25 15:32:26 +0530

    added LMFeedPostImageMediaView for post view as well as for create and edit

---
+++ likeminds-feed-android-core/src/main/java/com/likeminds/feed/android/core/ui/widgets/post/postmedia/view/LMFeedPostImageMediaView.kt
@@ -0,0 +1,54 @@
+package com.likeminds.feed.android.core.ui.widgets.post.postmedia.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.likeminds.feed.android.core.databinding.LmFeedPostImageMediaViewBinding
+import com.likeminds.feed.android.core.ui.base.styles.LMFeedIconStyle
+import com.likeminds.feed.android.core.ui.base.styles.LMFeedImageStyle
+import com.likeminds.feed.android.core.ui.base.styles.setStyle
+import com.likeminds.feed.android.core.ui.widgets.post.postmedia.style.LMFeedPostImageMediaViewStyle
+import com.likeminds.feed.android.core.utils.LMFeedViewUtils.hide
+import com.likeminds.feed.android.core.utils.LMFeedViewUtils.show
+
+class LMFeedPostImageMediaView : ConstraintLayout {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attributeSet: AttributeSet?) : super(context, attributeSet)
+
+    constructor(context: Context, attributeSet: AttributeSet?, defStyle: Int) : super(
+        context,
+        attributeSet,
+        defStyle
+    )
+
+    private val inflater =
+        (context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+
+    private val binding = LmFeedPostImageMediaViewBinding.inflate(inflater, this, true)
+
+    //sets provided [lmFeedPostImageMediaViewStyle] to the image view in the post
+    fun setStyle(lmFeedPostImageMediaViewStyle: LMFeedPostImageMediaViewStyle) {
+        lmFeedPostImageMediaViewStyle.apply {
+            configureImageView(imageStyle)
+            configureRemoveIconView(removeIconStyle)
+        }
+    }
+
+    private fun configureImageView(imageStyle: LMFeedImageStyle) {
+        binding.imageView.setStyle(imageStyle)
+    }
+
+    private fun configureRemoveIconView(removeIconStyle: LMFeedIconStyle?) {
+        binding.ivCross.apply {
+            if (removeIconStyle == null) {
+                hide()
+            } else {
+                show()
+                setStyle(removeIconStyle)
+            }
+        }
+    }
+}
